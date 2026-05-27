6ca78e6b272012fb488b7523583d3faa70979ced
ducky0203 <duckypham0203@gmail.com>
2026-05-27 10:39:57 +0700

    Release

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: 5555,
-  },
   preview: {
-    port: 5555,
+    host: true,
+    allowedHosts: ["qrgen.tre360.vn"],
+  },
+  server: {
+    host: true,
+    allowedHosts: ["qrgen.tre360.vn"],
   },
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
     tailwindcss(),
   ],
-})
+});
